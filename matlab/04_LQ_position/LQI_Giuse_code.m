32d5e7fd06b0bc87ab0509b5c75415db60573a54
gsac01 <gsacchi01@gmail.com>
2026-05-11 12:53:40 +0200

    Add files via upload

---
+++ matlab/04_LQ_position/LQI_Giuse_code.m
@@ -0,0 +1,166 @@
+clear
+clc
+%% 0) Parametri 
+Kg = 3; % gain amplificatore
+
+% Circuito LR
+Lc = 412.5e-3; %[H] inductance of the coil
+Rc = 10; %[ohm]
+% nc = 2450; %number of coils
+% lc = 0.0825; %[m] lenght of the coil
+% rc = 0.008; %[m] coil radius
+Rs = 1; %[ohm] current sense resistance
+Rcal = 0.85; % calibration resistance gain
+
+R_tot = (Rc + Rs)* Rcal; %[ohm] total resistance
+omega_coil = R_tot/Lc;
+
+% Meccanica
+km = 6.5308e-5; %[N-m^2/A^2] electromagnet force constant
+r = 1.27e-2; %[m] steel ball radius
+m = 0.068; %[kg] steel ball mass
+%Tb = 0.014; %[m] steel ball travel
+l = 0.0394; %[m] total lenght
+g = 9.81; %[m/s^2] gravity
+%mu_0 = pi*4e-7; %[H/m] magnetic permeability constant
+Kb = 2.83e-3; %[m/V] ball position sensitivity
+s = tf("s");
+I = [1, 0; 0, 1];
+
+%% 1) Modello Circuito LR
+
+%linear, SISO, Strictly proper, A.S., order 1
+A_i = -R_tot/Lc; 
+B_i = 1/Lc; %system is reachable
+C_i = 1; %system is observable 
+D_i = 0; 
+
+SS_i = ss(A_i, B_i, C_i, D_i);
+[G_i_num, G_i_den] = ss2tf(A_i, B_i, C_i, D_i);
+G_i = tf(G_i_num, G_i_den);
+G_i = Kg*G_i;
+[G_i_num, G_i_den] = tfdata(G_i, 'v');
+G_i
+fprintf("Poles of G_i = \n");
+pole(G_i)
+fprintf("Zeros of G_i = \n");
+zero(G_i)
+
+%figure
+%bode(G_i)
+%grid on
+%title('Diagramma di Bode di G_i')
+
+%figure
+%nyquist(G_i)
+%grid on
+%title('Diagramma di Nyquist di G_i')
+
+%% 2) Modello Forze
+
+%equilibrium
+x2_e = 0; 
+x1_e = 0.007; %eq position "middle of the airgap"
+u_e = (((g*m)/km)^(1/2))*(l - x1_e - (2*r));
+x_e = [x1_e, x2_e]; %equilibrium states vector
+
+
+%u_e = ;
+%x1_e = - ((km/(g*m))^(1/2)) - (2*r) + l;
+
+%Linearized model, SISO, Strictly proper, Unstable, order 2
+A_p = [0, 1; (2*km*(u_e^2))/(m*((l - x1_e - (2*r)))^3) 0]; 
+B_p = [0; (2*km*u_e)/(m*((l - x1_e - (2*r)))^2)];
+C_p = [1, 0];
+D_p = 0;
+
+SS_p = ss(A_p, B_p, C_p, D_p);
+G_p = tf(SS_p);
+[G_p_num, G_p_den] = tfdata(G_p, 'v');
+G_p
+fprintf("Poles of G_p = \n");
+pole(G_p)
+fprintf("Zeros of G_p = \n");
+zero(G_p)
+
+%checking reachability of linearized system
+Mr = ctrb(A_p,B_p);
+if (rank(Mr) == 2)
+    disp('The linearized model is reachable \n')
+else 
+    disp('The linearized model is not reachable \n');
+end
+
+%checking observability of linearized system
+Mo = obsv(A_p,C_p);
+if (rank(Mo) == 2)
+    disp('The linearized model is observable \n')
+else 
+    disp('The linearized model is not observable \n');
+end
+
+%% 3) Regolatore Corrente PI
+
+p = pole(G_p); % poles of the position plant
+wm = abs(p(1));
+wc = 10*wm;
+% wc = 150;
+phase_m = 90; %margine di fase in deg
+fprintf("Requirements: wc=%f , phase margin=%f \n", wc, phase_m);
+phiG_deg = -atand((wc*Lc)/R_tot);%fase a omega c
+Ti = 1/(wc * tand(180 + phiG_deg -phase_m));
+Ki_p = sqrt(R_tot^2 + (wc*Lc)^2) / ( Kg * sqrt(1 + 1/(wc^2 * Ti^2)) );
+Ki_i = Ki_p/Ti;
+fprintf("Kp = %f\n", Ki_p);
+fprintf("Ki = %f\n", Ki_i);
+
+R_i = Ki_p + Ki_i/s %current controller PI
+[R_i_num, R_i_den] = tfdata(R_i, 'v');
+
+L_i = G_i*R_i; %current open loop tf
+L_i = minreal(L_i)
+fprintf("Poles of L_i = \n");
+pole(L_i)
+fprintf("Zeros of L_i = \n");
+zero(L_i)
+[gm, pm, wcg, wcp_i] = margin(L_i);
+fprintf("Gain Margin = %f ,Phase Margin = %f ,w_c = %f \n", gm, pm, wcp_i);
+
+H_i = feedback(L_i, 1); %current closed loop tf
+H_i = minreal(H_i)
+fprintf("Poles of H_i = \n");
+pole(H_i)
+fprintf("Zeros of H_i = \n");
+zero(H_i)
+bw_i = bandwidth(H_i);
+fprintf("Bandwidth of H_i = %f \n", bw_i);
+
+% figure
+% step(H_i)
+% hold on
+% plot(-1, 0, 'rx', 'MarkerSize', 14, 'LineWidth', 2)
+% title('Step response of H_i')
+% grid on
+
+%%
+
+Q = diag([10, 10, 50]);
+R = 1;
+N = [0; 0; 0];
+[K,S,e] = lqi(SS_p, Q, R, N);
+
+Qn = diag([1e-10, 1e-6]);
+Rn = 1e-4;   
+sys_kalman = ss(A_p, [B_p, eye(2)], C_p, [0, 0, 0]);
+
+
+[kalmf, L, P] = kalman(sys_kalman, Qn, Rn);
+fprintf("Kalman gains L =\n"); disp(L)
+
+A_obs = A_p - L*C_p;
+B_obs = [B_p, L];
+C_obs = eye(2);
+D_obs = zeros(2, 2);
+SS_obs = ss(A_obs, B_obs, C_obs, D_obs);
+
+
